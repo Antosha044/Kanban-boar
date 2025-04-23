c2225138b7133b998150f62b281a1bde6dbe0356
Anton Kostil <v999253@gmail.com>
2025-04-23 23:09:52 +0300

    add migrations

--- src/settings.py
+++ src/settings.py
@@ -15,9 +15,13 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    @property
+    def DATABASE_URL_sync(self):
+        return f"postgresql+psycopg2://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    class Config:
-        env_file = ".env"
+
+model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
