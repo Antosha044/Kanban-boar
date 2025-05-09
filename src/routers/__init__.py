3bcc4c8965c1e67f5ed263aa9b856f7d3f92ed3e
Anton Kostil <v999253@gmail.com>
2025-05-09 21:38:00 +0300

    add project_router.py

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import auth_router
+from . import auth_router, project_router
 
 router = APIRouter()
 
 router.include_router(auth_router.router, prefix="/auth", tags=["auth"])
+router.include_router(project_router.router, prefix="/projects", tags=["projects"]) 
