44abee89c965123c7f9a9c67984a72e89ae435bb
Anton Kostil <v999253@gmail.com>
2025-05-10 22:25:29 +0300

    changed the token`s lifetime

--- src/core/jwt_utils.py
+++ src/core/jwt_utils.py
@@ -6,7 +6,7 @@ from src.settings import settings
 SECRET_KEY = settings.SECRET_KEY
 
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60  
+ACCESS_TOKEN_EXPIRE_MINUTES = 120 
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
